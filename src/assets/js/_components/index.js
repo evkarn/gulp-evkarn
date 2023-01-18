5fbed7cb25b23810cbaf764f3652120436723dc9
evkarn <evkarn@yandex.ru>
2023-01-18 12:57:15 +0500

    Обновление

--- src/assets/js/_components/index.js
+++ src/assets/js/_components/index.js
@@ -1,53 +1,55 @@
-export {adaptiveSpoilers} from "./adaptive-spoilers/adaptiveSpoilers.js";
+export { adaptiveSpoilers } from './adaptive-spoilers/adaptiveSpoilers.js';
 
-export {buttonToTop} from "./button-to-top/buttonToTop.js";
+export { buttonToTop } from './button-to-top/buttonToTop.js';
 
-export {colorScheme} from "./color-scheme/colorScheme.js";
+export { colorScheme } from './color-scheme/colorScheme.js';
 
-export {customTabs} from "./tabs-custom/customTabs.js";
+export { customTabs } from './tabs-custom/customTabs.js';
 
-export {customSelectDisplayNone} from "./select-display-none/customSelectDisplayNone.js";
+export { customSelectDisplayNone } from './select-display-none/customSelectDisplayNone.js';
 
-export {customSelectSmooth} from "./select-smooth/customSelectSmooth.js";
+export { customSelectSmooth } from './select-smooth/customSelectSmooth.js';
 
-export {disableScroll} from "./general-events/disable-scroll.js";
+export { disableScroll } from './general-events/disable-scroll.js';
 
-export {documentClick} from "./general-events/document-click.js";
+export { documentClick } from './general-events/document-click.js';
 
-export {enableScroll} from "./general-events/enable-scroll.js";
+export { enableScroll } from './general-events/enable-scroll.js';
 
-export {keydownEsc} from "./general-events/keydown-27-esc.js";
+export { keydownEsc } from './general-events/keydown-27-esc.js';
 
-export {digitalCounters} from "./digital-counters/digitalCounters.js";
+export { digitalCounters } from './digital-counters/digitalCounters.js';
 
-export {header} from "./header/header.js";
+export { header } from './header/header.js';
 
-export {imageInBackground} from "./image-in-background/imageInBackground.js";
+export { imageInBackground } from './image-in-background/imageInBackground.js';
 
-export {lazyLoadElement} from "./lazy-load/lazyLoadElement.js";
+export { lazyLoadElement } from './lazy-load/lazyLoadElement.js';
 
-export {lazyLoadImages} from "./lazy-load/lazyLoadImages.js";
+export { lazyLoadImages } from './lazy-load/lazyLoadImages.js';
 
-export {lazyLoadGoogleMap} from "./lazy-load/lazyLoadGoogleMap.js";
+export { lazyLoadGoogleMap } from './lazy-load/lazyLoadGoogleMap.js';
 
-export {lazyLoadYandexMap} from "./lazy-load/lazyLoadYandexMap.js";
+export { lazyLoadYandexMap } from './lazy-load/lazyLoadYandexMap.js';
 
-export {linkScrollToElement} from "./link-scroll-to-element/linkScrollToElement.js";
+export { linkScrollToElement } from './link-scroll-to-element/linkScrollToElement.js';
 
-export {modalEvkarnInit} from "./modal-evkarn/modalEvkarn.js";
+export { modalEvkarnInit } from './modal-evkarn/modalEvkarn.js';
 
-export {offsetBottomPanelPhone} from "./offset-bottom-panel-phone/offsetBottomPanelPhone.js";
+export { offsetBottomPanelPhone } from './offset-bottom-panel-phone/offsetBottomPanelPhone.js';
 
-export {portfolioTabs} from "./portfolio-tabs/portfolioTabs.js";
+export { portfolioTabs } from './portfolio-tabs/portfolioTabs.js';
 
-export {quiz} from "./quiz/quiz.js";
+export { quiz } from './quiz/quiz.js';
 
-export {ratingJS} from "./rating/ratingJS.js";
+export { ratingJS } from './rating/ratingJS.js';
 
-export {readProgressCircle} from "./read-progress-circle/readProgressCircle.js";
+export { readProgressCircle } from './read-progress-circle/readProgressCircle.js';
 
-export {readProgressLine} from "./read-progress-line/readProgressLine.js";
+export { readProgressLine } from './read-progress-line/readProgressLine.js';
 
-export {searchFieldGoogle} from "./searchFieldGoogle/searchFieldGoogle.js";
+export { searchFieldGoogle } from './searchFieldGoogle/searchFieldGoogle.js';
 
-export {webpFind} from "./webp-find/webpFind.js";
+export { showHidePassword } from './input-password-show-hide/input-password-show-hide.js';
+
+export { webpFind } from './webp-find/webpFind.js';
---
+++ src/assets/js/_components/input-password-show-hide/input-password-show-hide.js
@@ -0,0 +1,25 @@
+export function showHidePassword() {
+	'use strict';
+
+	const buttonsShowHidePass = document?.querySelectorAll('[data-show-hide-password]');
+
+	if (buttonsShowHidePass.length > 0) {
+		buttonsShowHidePass.forEach(el => {
+			el.addEventListener('click', (e) => {
+				const target = e.target;
+
+				target.classList.toggle('view-password');
+
+				const targetParent = target.parentNode;
+
+				const targetInput = targetParent.querySelector('input');
+
+				if (targetInput.getAttribute('type') === 'password') {
+					targetInput.setAttribute('type', 'text');
+				} else {
+					targetInput.setAttribute('type', 'password');
+				}
+			});
+		});
+	}
+}
