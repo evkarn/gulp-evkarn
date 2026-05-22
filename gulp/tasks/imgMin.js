48b40b04227e3ad53555bb9fe2af2b53093bb84f
Евгений Карнаухов <evkarn@yandex.ru>
2026-05-22 19:05:23 +0500

    Update

--- gulp/tasks/imgMin.js
+++ gulp/tasks/imgMin.js
@@ -1,65 +1,43 @@
-// Обработка ошибок
+// imgMin.js - более простой вариант с merge-stream
 import plumberInit from './plumber.js';
 import imageminWebp from 'imagemin-webp';
 import imageminAvif from 'imagemin-avif';
+import merge from 'merge-stream';
 
 export default function imgMin() {
-	// Находим исходники изображений
-	return (
-		app.gulp
-			.src(app.path.src.img, { encoding: false })
+	// Поток для WebP
+	const webpStream = app.gulp
+		.src(app.path.src.img, { encoding: false })
+		.pipe(app.plugins.plumber(plumberInit('IMAGES')))
+		.pipe(app.plugins.newer(app.path.build.img))
+		.pipe(app.plugins.imagemin([imageminWebp({ quality: 80 })], { verbose: true }))
+		.pipe(app.plugins.rename({ extname: '.webp' }))
+		.pipe(app.gulp.dest(app.path.build.img));
 
-			// Выдаём сообщение об ошибке, если она есть
-			.pipe(app.plugins.plumber(plumberInit('IMAGES')))
+	// Поток для AVIF
+	const avifStream = app.gulp
+		.src(app.path.src.img, { encoding: false })
+		.pipe(app.plugins.plumber(plumberInit('IMAGES')))
+		.pipe(app.plugins.newer(app.path.build.img))
+		.pipe(app.plugins.imagemin([imageminAvif({ quality: 80 })], { verbose: true }))
+		.pipe(app.plugins.rename({ extname: '.avif' }))
+		.pipe(app.gulp.dest(app.path.build.img));
 
-			// Проверяем менялись ли изображения и создаём дубликаты изображений в формате .webp
-			.pipe(app.plugins.newer(app.path.build.img))
-			.pipe(
-				app.plugins.imagemin(
-					[
-						imageminWebp({
-							quality: 80,
-						}),
-					],
-					{ verbose: true },
-				),
-			)
-			.pipe(app.plugins.rename({ extname: '.webp' }))
-			.pipe(app.gulp.dest(app.path.build.img))
+	// Поток для оптимизации
+	const optimizeStream = app.gulp
+		.src(app.path.src.img, { encoding: false })
+		.pipe(app.plugins.plumber(plumberInit('IMAGES')))
+		.pipe(app.plugins.newer(app.path.build.img))
+		.pipe(app.plugins.imagemin({
+			progressive: true,
+			interlaced: true,
+			quality: 80,
+			optimizationLevel: 3,
+			verbose: true,
+		}))
+		.pipe(app.gulp.dest(app.path.build.img))
+		.pipe(app.plugins.browsersync.stream());
 
-			// Проверяем менялись ли изображения и создаём дубликаты изображений в формате .avif
-			.pipe(app.gulp.src(app.path.src.img, { encoding: false }))
-			.pipe(app.plugins.newer(app.path.build.img))
-			.pipe(
-				app.plugins.imagemin(
-					[
-						imageminAvif({
-							quality: 80,
-						}),
-					],
-					{ verbose: true },
-				),
-			)
-			.pipe(app.plugins.rename({ extname: '.avif' }))
-			.pipe(app.gulp.dest(app.path.build.img))
-
-			// Если режим продакшена оптимизируем изображения
-			.pipe(app.gulp.src(app.path.src.img, { encoding: false }))
-			.pipe(app.plugins.newer(app.path.build.img))
-			.pipe(
-				app.plugins.imagemin({
-					progressive: true,
-					interlaced: true,
-					quality: 80,
-					optimizationLevel: 3, // 0 to 7
-					verbose: true,
-				}),
-			)
-
-			// Выгружаем изображения в папку assets/images
-			.pipe(app.gulp.dest(app.path.build.img))
-
-			// Перезагружаем страницу
-			.pipe(app.plugins.browsersync.stream())
-	);
+	// Объединяем все потоки в один
+	return merge(webpStream, avifStream, optimizeStream);
 }
